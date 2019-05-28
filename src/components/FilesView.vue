8225f7a829011aed03cfd11bee4738544e3bed85
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-05-28 23:59:56 +0300

    Refactoring

--- src/components/FilesView.vue
+++ src/components/FilesView.vue
@@ -3,18 +3,22 @@
     <v-text-field
         label="Doc/Dir name"
         required
+        v-model="path"
     ></v-text-field>
     <v-text-field
         label="Postfix"
         required
+        v-model="postfix"
     ></v-text-field>
-    <v-btn small outline color="indigo" class="ml-0">Search</v-btn>
-    <ul>
+    <v-btn small outline color="indigo" class="ml-0" v-on:click="getFiles">Search</v-btn>
+    <v-divider class="mt-3 mb-3"></v-divider>
+    <ul v-if="!isError">
       <tree-item
           class="item"
           :item="treeData"
       ></tree-item>
     </ul>
+    <p v-if="isError" class="red--text"> {{ errMessage }}</p>
   </div>
 </template>
 
@@ -31,16 +35,27 @@
                 treeData: {},
                 path: '',
                 postfix: '',
+                isError: false,
+                errMessage: ''
             }
         },
         created() {
-            this.$http.post('http://192.168.1.4:8080/files', {
-                path: '/home/da-nizz/IdeaProjects/text-search-engine',
-                postfix: ''
-            }).then(response => {
-                    this.treeData = response.body;
-                }
-            )
+
+        },
+        methods: {
+            getFiles: function () {
+                this.isError = false;
+                this.$http.post('http://localhost:8080/files', {
+                    path: this.path,
+                    postfix: this.postfix
+                }).then(response => {
+                        this.treeData = response.body;
+                    }, response => {
+                        this.isError = true;
+                        this.errMessage = response.body.message;
+                    }
+                )
+            }
         }
     }
 </script>
